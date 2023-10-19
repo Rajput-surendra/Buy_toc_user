3f83fe1ea072a06dd932394b20b36a7f5506fe82
Rajput-surendra <surendraalphawizz@gmail.com>
2023-10-19 20:57:52 +0530

    Live Code

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,21 @@
+final String appName = 'Buybtoc';
+
+final String packageName = 'com.buybtoc.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.buybtoc.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.buybtoc.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+// final String baseUrl = 'https://zuqzuq.com/app/v1/api/';
+// final String baseUrl = 'https://developmentalphawizz.com/buybtoc/app/v1/api/';
+final String baseUrl = 'https://buybtoc.com/app/v1/api/';
+final String imageUrl = 'https://buybtoc.com/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
